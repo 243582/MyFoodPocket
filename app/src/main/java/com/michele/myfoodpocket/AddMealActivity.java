d273bac0200b2eb9e148da2fbadf1a9d4834ce2f
Michele Zaza <mzaza998@gmail.com>
2021-12-24 10:18:11 +0100

    Commit 21: ora AddMealActivity permette di scattare una foto al pasto, caricarla su firebase storage, caricare il percorso su firebase (se è nullo viene caricato "none")

--- app/src/main/java/com/michele/myfoodpocket/AddMealActivity.java
+++ app/src/main/java/com/michele/myfoodpocket/AddMealActivity.java
@@ -2,10 +2,14 @@ package com.michele.myfoodpocket;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
@@ -19,10 +23,23 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class AddMealActivity extends AppCompatActivity {
+
+    // Variabili per scattare foto e memorizzarla
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private String currentPhotoPath;
+    private Bitmap photo;
+    private String photoPath = "none"; // Inizializzo il percorso della foto a "none": se verrà scattata verrà sostituito con il vero percorso, altrimenti rimarrà "none"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +64,7 @@ public class AddMealActivity extends AppCompatActivity {
         EditText editTextDescription = (EditText)(findViewById(R.id.add_meal_edit_text_description));
         EditText editTextCalories = (EditText)(findViewById(R.id.add_meal_edit_text_calories));
 
-        Meal newMeal = new Meal(emailDate, category, editTextDescription.getText().toString(), Integer.parseInt(editTextCalories.getText().toString()));
+        Meal newMeal = new Meal(emailDate, category, editTextDescription.getText().toString(), Integer.parseInt(editTextCalories.getText().toString()), photoPath);
         databaseReference.child("Meal").push().setValue(newMeal).addOnSuccessListener(new OnSuccessListener<Void>()
         {
             @Override
@@ -74,10 +91,69 @@ public class AddMealActivity extends AppCompatActivity {
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Create the File where the photo should go
+        File photoFile = null;
         try {
-            startActivityForResult(takePictureIntent, 1);
-        } catch (ActivityNotFoundException e) {
-            // display error state to the user
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            // Error occurred while creating the File
+        }
+        // Continue only if the File was successfully created
+        if (photoFile != null) {
+            Uri photoURI = FileProvider.getUriForFile(this,
+                    "com.michele.myfoodpocket.fileprovider",
+                    photoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference();
+
+            // [START upload_file]
+            Uri file = Uri.fromFile(new File(currentPhotoPath));
+            StorageReference riversRef = storageRef.child("Images/" + file.getLastPathSegment());
+            photoPath = "Images/" + file.getLastPathSegment();
+            UploadTask uploadTask;
+            uploadTask = riversRef.putFile(file);
+
+            // Register observers to listen for when the download is done or if it fails
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                    // ...
+                }
+            });
+            // [END upload_file]
         }
     }
 }
--- app/src/main/java/com/michele/myfoodpocket/Meal.kt
+++ app/src/main/java/com/michele/myfoodpocket/Meal.kt
@@ -5,14 +5,16 @@ class Meal {
     private var category : String = "";
     private var description : String = "";
     private var calories : Int = 0; // Calorie provvisorie che verranno poi sostituite con quelle del pasto
+    private var photoPath : String = "";
 
     constructor() {}
 
-    constructor(emailDate : String, category : String, description: String, calories : Int) {
+    constructor(emailDate : String, category : String, description: String, calories : Int, photoPath : String) {
         this.emailDate = emailDate;
         this.category = category;
         this.description = description;
         this.calories = calories;
+        this.photoPath = photoPath;
     }
 
     override fun toString() : String {
@@ -52,6 +54,15 @@ class Meal {
 
     fun setCalories(calories : Int) : Void? {
         this.calories = calories;
+        return null;
+    }
+
+    fun getPhotoPath() : String {
+        return this.photoPath;
+    }
+
+    fun setPhotoPath(description : String) : Void? {
+        this.photoPath = photoPath;
         return null;
     }
 }
